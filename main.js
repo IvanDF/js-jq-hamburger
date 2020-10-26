a760166d8c4eecf2033b54ff9b898895c71a9642
Ivan <delfatti.ivan@gmail.com>
2020-10-26 14:43:43 +0100

    Testing

--- main.js
+++ main.js
@@ -5,20 +5,60 @@
  * 
  */
 
-//  VARIABILI
- var iconHamburger = $('.fa-bars');
- var iconHamburgerExit = $('.fa-times');
- var hamburgerMenu = $('.hamburger-menu')
 
-//  OPEN HAMBURGER MENU
-iconHamburger.click( function(){
-    hamburgerMenu.addClass('active');
+/************************************
+ *  jQuery
+ ***********************************/
+
+//  VARIABILI jQuery
+ var openMenu = $('.fa-bars');
+var closeMenu =  $('.fa-times');
+var menu = $('.hamburger-menu')
+
+/**
+ * SENZA TRASNSIZIONE
+ */
+
+//  APRI HAMBURGER MENU
+openMenu.click( function(){
+    menu.addClass('active');
 })
 
-// CLOSE HAMBURGER MENU
-iconHamburgerExit.click( function(){
-    hamburgerMenu.removeClass('active');
+//  CHIUDI HAMBURGER MENU
+closeMenu.click( function(){
+    menu.removeClass('active');
 })
 
 
+// /**
+//  * CON TRASNSIZIONE
+//  */
+// //  APRI HAMBURGER MENU
+// openMenu.click( function(){
+//     menu.fadeIn();
+// })
+// //  CHIUDI HAMBURGER MENU
+// closeMenu.click( function(){
+//     menu.fadeOut();
+// })
 
+
+
+
+
+/************************************
+ *  JavaScript
+ ***********************************/
+
+// //  VARIABILI JS
+// var openMenu = document.getElementsByClassName('fa-bars');
+// var closeMenu = document.getElementsByClassName('fa-times');
+// var menu = document.getElementsByClassName('hamburger-menu');
+
+// // FUNZIONE TOGGLE HAMBURGER MENU
+// openMenu[0].addEventListener('click' , function(){
+//     menu[0].className += ' active';
+// })
+// closeMenu[0].addEventListener('click' , function(){
+//     menu[0].className = 'hamburger-menu';
+// })
