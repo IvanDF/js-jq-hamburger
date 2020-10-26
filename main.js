976fabb97c45507767d0d5714d1d4f6a6acde71e
Ivan <delfatti.ivan@gmail.com>
2020-10-26 14:59:06 +0100

    Fixes

--- main.js
+++ main.js
@@ -11,39 +11,36 @@
  ***********************************/
 
 //  VARIABILI jQuery
- var openMenu = $('.fa-bars');
+var openMenu = $('.fa-bars');
 var closeMenu =  $('.fa-times');
 var menu = $('.hamburger-menu')
 
-/**
- * SENZA TRASNSIZIONE
- */
-
-//  APRI HAMBURGER MENU
-openMenu.click( function(){
-    menu.addClass('active');
-})
-
-//  CHIUDI HAMBURGER MENU
-closeMenu.click( function(){
-    menu.removeClass('active');
-})
-
-
 // /**
-//  * CON TRASNSIZIONE
+//  * SENZA TRASNSIZIONE
 //  */
+
 // //  APRI HAMBURGER MENU
 // openMenu.click( function(){
-//     menu.fadeIn();
+//     menu.addClass('active');
 // })
+
 // //  CHIUDI HAMBURGER MENU
 // closeMenu.click( function(){
-//     menu.fadeOut();
+//     menu.removeClass('active');
 // })
 
 
-
+/**
+ * CON TRASNSIZIONE
+ */
+//  APRI HAMBURGER MENU
+openMenu.click( function(){
+    menu.fadeIn(2000);
+})
+//  CHIUDI HAMBURGER MENU
+closeMenu.click( function(){
+    menu.fadeOut(1000);
+})
 
 
 /************************************
@@ -51,14 +48,14 @@ closeMenu.click( function(){
  ***********************************/
 
 // //  VARIABILI JS
-// var openMenu = document.getElementsByClassName('fa-bars');
-// var closeMenu = document.getElementsByClassName('fa-times');
-// var menu = document.getElementsByClassName('hamburger-menu');
+// var openMenu = document.getElementsByClassName('fa-bars')[0];
+// var closeMenu = document.getElementsByClassName('fa-times')[0];
+// var menu = document.getElementsByClassName('hamburger-menu')[0];
 
 // // FUNZIONE TOGGLE HAMBURGER MENU
-// openMenu[0].addEventListener('click' , function(){
-//     menu[0].className += ' active';
+// openMenu.addEventListener('click' , function(){
+//     menu.className += ' active';
 // })
-// closeMenu[0].addEventListener('click' , function(){
-//     menu[0].className = 'hamburger-menu';
+// closeMenu.addEventListener('click' , function(){
+//     menu.className = 'hamburger-menu';
 // })
